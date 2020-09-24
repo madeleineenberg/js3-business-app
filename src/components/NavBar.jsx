4abce2ebab56ef3918f37a65641b5813453466a0
madeleineenberg <madeleine_enberg@hotmail.com>
2020-09-24 20:40:59 +0200

    create Home link in nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import UserKit from "../data/UserKit";
 import { Nav, NavList, NavItem, NavLink } from "../styled/Nav";
 import { Logo } from "../styled/Heading";
 
 export default function NavBar({ children }) {
+  const userKit = new UserKit();
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    getToken();
+  }, [getToken]);
+
+  function getToken() {
+    setToken(userKit.getToken());
+  }
   return (
     <Nav>
       <Logo>Business Project</Logo>
       <NavList>
         <NavItem>
-          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/">REGISTRATION</NavLink>
         </NavItem>
         <NavItem>
           <NavLink to="/login">LOGIN</NavLink>
         </NavItem>
+        {token && (
+          <NavItem>
+            <NavLink to="/home">HOME</NavLink>
+          </NavItem>
+        )}
       </NavList>
 
       {children}
